49e1c512b823cccb24d9ea88a5bdf7071a1c0e48
PhilippeItsMe <144894663+PhilippeItsMe@users.noreply.github.com>
2025-11-06 15:37:49 +0100

    dev - accessiblité setup on start page

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,6 +16,7 @@ toggle.addEventListener('change', () => {
         subtitle.style.color = 'var(--grey500)';
         for (let i = 0; i < buttons.length; i++) {
             buttons[i].style.backgroundColor = 'var(--white)';
+            buttons[i].style.border = '2px solid var(--white)';
         }
         for (let i = 0; i < subjects.length; i++) {
             subjects[i].style.color = 'var(--blue900)';
@@ -29,9 +30,11 @@ toggle.addEventListener('change', () => {
         subtitle.style.color = 'var(--blue300)';
         for (let i = 0; i < buttons.length; i++) {
             buttons[i].style.backgroundColor = 'var(--blue850)';
+            buttons[i].style.border = '2px solid var(--blue850)';
         }
         for (let i = 0; i < subjects.length; i++) {
             subjects[i].style.color = 'var(--white)';
+            
         }
     }
 });
